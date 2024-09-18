98ce04ba2728804d667dfc82023d1763a9770719
xperiatechlk <xperiatech.lk@gmail.com>
2024-09-18 18:25:30 +0530

    adding cors policy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors"); 0
 dotenv.config();
 const app = express();
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://xperia.vercel.app'], 
+    origin: ['http://localhost:3000', 'https://xperia.vercel.app', 'https://xperia-backend.vercel.app'],
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
 };
